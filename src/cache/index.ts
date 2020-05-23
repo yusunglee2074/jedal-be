96ae666751b9bfd11a2407fe1c0fc41c182e16ed
yusunglee2074 <lys0333@gmail.com>
2020-05-23 12:51:42 +0900

    처음 캐쉬 데이터 만들 때 캐쉬데이터 만든 후 TrimmedRecipe 컬렉션도 자동으로 만들어서 업데이트

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,8 +1,12 @@
 import * as NodeCache from 'node-cache';
 import axios from 'axios';
 import * as fs from 'fs';
+import { changeOpenApiObjKey } from '../utils';
 
-enum openApiType {
+export const OPEN_API_BASE_URL =
+  'http://211.237.50.150:7080/openapi/356177e65657d63ea1189bb06144ce2d8035cd8b1434845e92abd7b7afe18b52/json';
+
+export enum openApiType {
   recipe = 'Grid_20150827000000000226_1',
   ingredient = 'Grid_20150827000000000227_1',
   seasonIngredient = 'Grid_20171128000000000572_1',
@@ -14,16 +18,14 @@ export const openApiCache = new NodeCache({
 });
 
 const getOpenApiData = async (apiType: openApiType): Promise<[]> => {
-  const baseUrl =
-    'http://211.237.50.150:7080/openapi/356177e65657d63ea1189bb06144ce2d8035cd8b1434845e92abd7b7afe18b52/json';
   try {
-    const res = await axios.get(baseUrl + '/' + apiType + '/1/1000');
+    const res = await axios.get(OPEN_API_BASE_URL + '/' + apiType + '/1/1000');
     let rows = res.data[apiType].row;
     const totalCount = res.data[apiType].totalCnt;
     let endRow = 1000;
     while (endRow < totalCount) {
       endRow += 1000;
-      const res = await axios.get(baseUrl + '/' + apiType + `/${endRow - 999}/${endRow}`);
+      const res = await axios.get(OPEN_API_BASE_URL + '/' + apiType + `/${endRow - 999}/${endRow}`);
       rows = rows.concat(res.data[apiType].row);
     }
     return rows;
@@ -38,6 +40,8 @@ const offlineData = fs.readFileSync(__dirname + '/openApiData.txt', 'utf8');
 const parsedOfflineData = JSON.parse(offlineData);
 
 const isDataFresh = (): boolean => {
+  console.log(parsedOfflineData.createdAt);
+  if (!parsedOfflineData.createdAt) return false;
   const yesterday = new Date();
   yesterday.setDate(new Date().getDate() - 1);
   return yesterday.getTime() < new Date(parsedOfflineData.createdAt).getTime();
@@ -51,14 +55,16 @@ const setCacheFromOfflineFile = (): void => {
 };
 
 const setCacheDataAndUpdateFileFromOnline = async (): Promise<void> => {
+  // TODO: 만약 성능이 만약 부족하다면 아이디로 찾는 재료, 디테일 레시피는
+  //  리스트가 아닌 obj 로 저장하므로써 indexing 도 생각해봄
   const recipes = await getOpenApiData(openApiType.recipe);
   const detailRecipes = await getOpenApiData(openApiType.detailRecipe);
   const ingredients = await getOpenApiData(openApiType.ingredient);
   const seasonIngredients = await getOpenApiData(openApiType.seasonIngredient);
-  openApiCache.set('recipes', recipes);
-  openApiCache.set('detailRecipes', detailRecipes);
-  openApiCache.set('ingredients', ingredients);
-  openApiCache.set('seasonIngredients', seasonIngredients);
+  openApiCache.set('recipes', changeOpenApiObjKey(recipes));
+  openApiCache.set('detailRecipes', changeOpenApiObjKey(detailRecipes));
+  openApiCache.set('ingredients', changeOpenApiObjKey(ingredients));
+  openApiCache.set('seasonIngredients', changeOpenApiObjKey(seasonIngredients));
   const offlineData = {
     createdAt: new Date().getTime(),
     recipes,
@@ -66,14 +72,14 @@ const setCacheDataAndUpdateFileFromOnline = async (): Promise<void> => {
     ingredients,
     seasonIngredients,
   };
-  // TODO: 에러 났을 때 다시  데이터 다운받을 수 있도록 createdAt 초기화 해야함
   fs.writeFileSync(__dirname + '/openApiData.txt', JSON.stringify(offlineData));
 };
 
-export const initCacheData = (): void => {
+export const initCacheData = async (): Promise<void> => {
   if (isDataFresh()) {
     setCacheFromOfflineFile();
   } else {
-    setCacheDataAndUpdateFileFromOnline();
+    await setCacheDataAndUpdateFileFromOnline();
   }
+  require(__dirname + '/../utils/initTrimmedRecipeCollection.ts');
 };
